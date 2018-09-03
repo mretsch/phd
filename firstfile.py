ef0052a65d52e61e0833e5fe0b6edb69a8770f2a
Matthias Retsch <matthias.retsch@monash.edu>
2018-09-03 18:39:03 +1000

    Calculate area and rain-rate ratios of convective and stratiform precip.

--- firstfile.py
+++ firstfile.py
@@ -1,7 +1,12 @@
 import math as m
 import xarray as xr
-from dask.distributed import Client
+import bottleneck as bn
+# from dask.distributed import Client
 import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+
+start = timeit.default_timer()
 import base_stats
 
 files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_season0910.nc"
@@ -24,24 +29,53 @@ rain_conv = rain.where(stein == 2)
 rain_stra = rain.where(stein == 1)
 
 
-# def total_area(lats, lons):
-#     """Calculate total area based on latitudes and longitudes. Neglects earth's curvature per rectangle."""
-#     earth_r = 6378000
-#     dist = earth_r * m.pi / 180.0
-#     lats_m = np.array(map(lambda lat: dist * lat, lats))
-#     lons_m = np.array(               [dist * lon * m.cos(m.radians(lat))
-#                                       for lat, lon in zip(lats, lons)])
-
-
 def total_area(array):
-    """Calculate total area of an array based on regular lat and lon grid."""
+    """Calculate total and single cell area of an array based on regular lat and lon grid."""
     lat_dist, lon_dist = 0.5 * (array.lat[1] - array.lat[0]), array.lon[1] - array.lon[0]
     stacked = array.stack(z=('lat', 'lon'))
-    valid = stacked.where(xr.ufuncs.isnan(stacked) == False, drop=True)
+    valid = stacked.where(stacked.notnull(), drop=True)
 
     earth_r = 6378000 # in metre
     large_wedge = m.pi * earth_r**2 * np.sin(np.deg2rad(abs(valid.z.lat) + lat_dist)) * (lon_dist / 180.0)
     small_wedge = m.pi * earth_r**2 * np.sin(np.deg2rad(abs(valid.z.lat) - lat_dist)) * (lon_dist / 180.0)
-    return (large_wedge - small_wedge).sum()
+    area = large_wedge - small_wedge
+    areas = area.unstack('z')
+    return area.sum(), areas
 
-scan_area = total_area(rain[40,:,:])
+
+area_scan, area_cell = total_area(rain[40, :, :])
+
+expanded = area_cell.expand_dims('time')
+expanded.time[0] = rain.time[0]
+bcasted, _ = xr.broadcast(expanded, rain)
+area_cell_all = bcasted.ffill('time')
+
+
+def nan_sum(array, axis):
+    """Summarise an xarray containing NaNs and which doesn't work with .sum(skipna=True)."""
+    return bn.nansum(array, axis=axis)
+
+
+def mask_sum(selector, background, per_timestep=False):
+    """Summarise an array after applying a mask to it."""
+    valid = background.where(selector.notnull())
+    if per_timestep:
+        valid_sum = valid.groupby('time').reduce(nan_sum)
+    else:
+        valid_sum = bn.nansum(valid)
+    return valid_sum
+
+
+conv_area = mask_sum(rain_conv, area_cell_all, per_timestep=True) / area_scan
+stra_area = mask_sum(rain_stra, area_cell_all, per_timestep=True) / area_scan
+
+conv_rain = rain_conv.groupby('time').reduce(nan_sum)
+stra_rain = rain_stra.groupby('time').reduce(nan_sum)
+
+# xarray automatically throws NaN if division by zero
+# TODO: Handle the case that there's conv precip, but no stratiform.
+conv_stra_area = conv_area / stra_area
+conv_stra_rain = conv_rain / stra_rain
+
+stop = timeit.default_timer()
+print('Run Time: ', stop - start)
