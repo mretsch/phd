3527d923f5bc919791a7d028f7219ca2ec59183c
Matthias Retsch <matthias.retsch@monash.edu>
2018-09-04 21:33:38 +1000

    Increased speed by using native xarray-methods instead of calling an own little function with groupby-objects.

--- firstfile.py
+++ firstfile.py
@@ -6,28 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import timeit
 
-start = timeit.default_timer()
-import base_stats
-
-files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_oneday.nc"
-ds_st = xr.open_mfdataset("/Users/mret0001/Data/"+files,chunks={'time':40})
-files = "RainRate/CPOL_RADAR_ESTIMATED_RAIN_RATE_oneday.nc"
-ds_rr = xr.open_mfdataset("/Users/mret0001/Data/"+files, chunks={'time':40})
-
-rain  = ds_rr.radar_estimated_rain_rate
-stein = ds_st.steiner_echo_classification
-
-# run with 4 parallel threads on my local laptop
-c = Client()
-
-# create an array which has the time-dim as 'height'. For ParaView.
-#   array = xr.DataArray(np.array(stein),coords=[('height',list(range(1,len(stein.time)+1))),('lat',stein.lat),('lon',stein.lon)])
-#   stein_ds = xr.Dataset({'SteinerClass':array})
-#   stein_ds.to_netcdf('height_steiner_data.nc')
-
-rain_conv = rain.where(stein == 2)
-rain_stra = rain.where(stein == 1)
-
 
 def total_area(array):
     """Calculate total and single cell area of an array based on regular lat and lon grid."""
@@ -43,43 +21,78 @@ def total_area(array):
     return area.sum(), areas
 
 
-area_scan, area_cell = total_area(rain[40, :, :])
-
-expanded = area_cell.expand_dims('time')
-expanded.time[0] = rain.time[0]
-bcasted, _ = xr.broadcast(expanded, rain)
-area_cell_all = bcasted.ffill('time')
-
-
 def nan_sum(array, axis):
-    """Summarise an xarray containing NaNs and which doesn't work with .sum(skipna=True)."""
+    """Sum an xarray containing NaNs and which doesn't work with .sum(skipna=True)."""
     return bn.nansum(array, axis=axis)
 
 
 def mask_sum(selector, background, per_timestep=False):
-    """Summarise an array after applying a mask to it."""
-    valid = background.where(selector.notnull())
+    """Sum an array after applying a mask to it."""
+    valid = background.where(selector.notnull()).fillna(0.)
     if per_timestep:
-        # calling nan_sum on the groupby('time') object
-        # takes 150 seconds at 4 processes for 3 days of data (time=432,lat=117,lon=117). Too long.
-        valid_sum = valid.groupby('time').reduce(nan_sum)
+        # calling nan_sum on the groupby('time') object here
+        # takes 150 seconds at 4 processes for 3 days of data (time=432,lat=117,lon=117). Too long -> .fillna(0.)
+        valid_sum = valid.groupby('time').sum()
     else:
         valid_sum = bn.nansum(valid)
     return valid_sum
 
 
-conv_area = mask_sum(rain_conv, area_cell_all, per_timestep=True) / area_scan
-stra_area = mask_sum(rain_stra, area_cell_all, per_timestep=True) / area_scan
+def precip_stats(rain, stein):
+    """Calculates area and rate of stratiform and convective precipitation and ratios between them."""
+    rain_conv = rain.where(stein == 2)
+    rain_stra = rain.where(stein == 1)
 
-conv_rain = rain_conv.groupby('time').reduce(nan_sum)
-stra_rain = rain_stra.groupby('time').reduce(nan_sum)
+    i = 0
+    while rain[i,:,:].isnull().all():
+        i += 1
+    else:
+        area_scan, area_cell = total_area(rain[i, :, :])
 
-# xarray automatically throws NaN if division by zero
-conv_stra_area = conv_area / stra_area
-conv_stra_rain = conv_rain / stra_rain
+    expanded = area_cell.expand_dims('time')
+    expanded.time[0] = rain.time[0]
+    bcasted, _ = xr.broadcast(expanded, rain)
+    area_cell_all = bcasted.ffill('time')
 
-conv_stra_area[xr.ufuncs.logical_and(conv_area > 0., stra_area == 0.)] = -1
-conv_stra_rain[xr.ufuncs.logical_and(conv_rain > 0., stra_rain == 0.)] = -1
+    conv_area = mask_sum(rain_conv, area_cell_all, per_timestep=True) / area_scan
+    stra_area = mask_sum(rain_stra, area_cell_all, per_timestep=True) / area_scan
+    if not conv_area._in_memory:
+        conv_area.load()
+        stra_area.load()
 
-stop = timeit.default_timer()
-print('Run Time: ', stop - start)
+    conv_rain = rain_conv.groupby('time').sum(skipna=True)
+    stra_rain = rain_stra.groupby('time').sum(skipna=True)
+    if not conv_rain._in_memory:
+        conv_rain.load()
+        stra_rain.load()
+
+    # xarray automatically throws NaN if division by zero
+    ratio_area = conv_area / stra_area
+    ratio_rain = conv_rain / stra_rain
+    ratio_area[xr.ufuncs.logical_and(conv_area > 0., stra_area == 0.)] = -1
+    ratio_rain[xr.ufuncs.logical_and(conv_rain > 0., stra_rain == 0.)] = -1
+
+    return ratio_area, ratio_rain, conv_rain, stra_rain
+
+
+if __name__ == '__main__':
+
+    start = timeit.default_timer()
+
+    files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_season0910.nc"
+    ds_st = xr.open_mfdataset("/Users/mret0001/Data/"+files, chunks={'time': 1000})
+    files = "RainRate/CPOL_RADAR_ESTIMATED_RAIN_RATE_season0910.nc"
+    ds_rr = xr.open_mfdataset("/Users/mret0001/Data/"+files, chunks={'time': 1000})
+
+    # run with 4 parallel threads on my local laptop
+    # c = Client()
+
+    # create an array which has the time-dim as 'height'. For ParaView.
+    #   array = xr.DataArray(np.array(stein),coords=[('height',list(range(1,len(stein.time)+1))),('lat',stein.lat),('lon',stein.lon)])
+    #   stein_ds = xr.Dataset({'SteinerClass':array})
+    #   stein_ds.to_netcdf('height_steiner_data.nc')
+
+    stats = precip_stats(rain=ds_rr.radar_estimated_rain_rate, stein=ds_st.steiner_echo_classification)
+
+    stop = timeit.default_timer()
+    print('Run Time: ', stop - start)
