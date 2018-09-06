Added functionality for basic statistics. Choose different periods for continuous aggregation (period) or grouping (group). Also corrected the calculation for rain rate by multiplying with the respective cell area and the calculation for rain area by not dividing by total area.

basic_stats.py
@@ -26,53 +26,64 @@ def nan_sum(array, axis):
     return bn.nansum(array, axis=axis)
 
 
-def mask_sum(selector, background, per_timestep=False):
+def mask_sum(selector, background, period='', group=''):
     """Sum an array after applying a mask to it."""
+    grouped = period == ''
+
     valid = background.where(selector.notnull()).fillna(0.)
-    if per_timestep:
+    if grouped:
         # calling nan_sum on the groupby('time') object here
         # takes 150 seconds at 4 processes for 3 days of data (time=432,lat=117,lon=117). Too long -> .fillna(0.)
-        valid_sum = valid.groupby('time').sum()
+        valid_sum = valid.groupby('time' + group).sum()
     else:
-        valid_sum = bn.nansum(valid)
+        valid_sum = valid.resample(time=period).sum()
     return valid_sum
 
 
-def precip_stats(rain, stein):
+def precip_stats(rain, stein, period='', group=''):
     """Calculates area and rate of stratiform and convective precipitation and ratios between them."""
+
+    grouped = period == ''
     rain_conv = rain.where(stein == 2)
     rain_stra = rain.where(stein == 1)
 
     i = 0
-    while rain[i,:,:].isnull().all():
+    while rain[i, :, :].isnull().all():
         i += 1
     else:
-        area_scan, area_cell = total_area(rain[i, :, :])
+        area_scan, area_cell_single = total_area(rain[i, :, :])
 
-    expanded = area_cell.expand_dims('time')
+    expanded = area_cell_single.expand_dims('time')
     expanded.time[0] = rain.time[0]
     bcasted, _ = xr.broadcast(expanded, rain)
-    area_cell_all = bcasted.ffill('time')
+    area_cell = bcasted.ffill('time')
 
-    conv_area = mask_sum(rain_conv, area_cell_all, per_timestep=True) / area_scan
-    stra_area = mask_sum(rain_stra, area_cell_all, per_timestep=True) / area_scan
+    # Multiply rain-rate (unit is kg/sm^2), with the respective grid cell area before summation.
+    if grouped:
+        group = group if (group == '') else '.' + group
+        conv_rain = (rain_conv * area_cell).groupby('time' + group).sum(skipna=True)
+        stra_rain = (rain_stra * area_cell).groupby('time' + group).sum(skipna=True)
+    else:
+        conv_rain = (rain_conv * area_cell).resample(time=period).sum(skipna=True)
+        stra_rain = (rain_stra * area_cell).resample(time=period).sum(skipna=True)
+
+    # The ratio toward the total scan area is not of interest, only absolute values and their ratio (calculated later).
+    # area_period = area_scan if (grouped) else area_scan * (len(rain.time) / len(conv_rain.time))
+    conv_area = mask_sum(rain_conv, area_cell, period, group) # / area_period
+    stra_area = mask_sum(rain_stra, area_cell, period, group) # / area_period
+
     if not conv_area._in_memory:
         conv_area.load()
         stra_area.load()
-
-    conv_rain = rain_conv.groupby('time').sum(skipna=True)
-    stra_rain = rain_stra.groupby('time').sum(skipna=True)
     if not conv_rain._in_memory:
         conv_rain.load()
         stra_rain.load()
 
     # xarray automatically throws NaN if division by zero
-    ratio_area = conv_area / stra_area
-    ratio_rain = conv_rain / stra_rain
-    ratio_area[xr.ufuncs.logical_and(conv_area > 0., stra_area == 0.)] = -1
-    ratio_rain[xr.ufuncs.logical_and(conv_rain > 0., stra_rain == 0.)] = -1
+    ratio_area = conv_area / (stra_area + conv_area)
+    ratio_rain = conv_rain / (stra_rain + conv_rain)
 
-    return ratio_area, ratio_rain, conv_rain, stra_rain
+    return conv_area, ratio_area, conv_rain, ratio_rain
 
 
 if __name__ == '__main__':
@@ -88,11 +99,16 @@ if __name__ == '__main__':
     # c = Client()
 
     # create an array which has the time-dim as 'height'. For ParaView.
-    #   array = xr.DataArray(np.array(stein),coords=[('height',list(range(1,len(stein.time)+1))),('lat',stein.lat),('lon',stein.lon)])
+    #   array = xr.DataArray(np.array(stein),
+    #                        coords=[('height',list(range(1,len(stein.time)+1))),('lat',stein.lat),('lon',stein.lon)])
     #   stein_ds = xr.Dataset({'SteinerClass':array})
     #   stein_ds.to_netcdf('height_steiner_data.nc')
 
-    stats = precip_stats(rain=ds_rr.radar_estimated_rain_rate, stein=ds_st.steiner_echo_classification)
+    # Rain rate units are mm/hour, dividing by 86400 yields mm/s == kg/m^2s
+    conv_area, ratio_area, conv_rain, ratio_rain = precip_stats(rain=ds_rr.radar_estimated_rain_rate / 86400.,
+                                                                stein=ds_st.steiner_echo_classification,
+                                                                # period='1H',
+                                                                group='hour')
 
     stop = timeit.default_timer()
     print('Run Time: ', stop - start)