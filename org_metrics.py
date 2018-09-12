6722b1fc1096c7df87c2afb3529d940eae1e6010
Matthias Retsch <matthias.retsch@monash.edu>
2018-09-12 14:51:26 +1000

    Input for skm.regionprops is now an integer array.

--- org_metrics.py
+++ org_metrics.py
@@ -7,15 +7,17 @@ import skimage.measure as skm
 
 start = timeit.default_timer()
 
-files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_threedays.nc"
+files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_oneday.nc"
 ds_st = xr.open_mfdataset("/Users/mret0001/Data/"+files, chunks={'time': 1000})
 
 stein = ds_st.steiner_echo_classification
 conv = stein.where(stein == 2)
 
-labeled = xr.zeros_like(conv).load()
+labeled = xr.zeros_like(conv).load().astype(int)
 for i, scene in enumerate(conv):
     labeled[i, :, :] = skm.label(scene)
+
+props = skm.regionprops(labeled)
 
 stop = timeit.default_timer()
 print('This script needed {} seconds.'.format(stop-start))
