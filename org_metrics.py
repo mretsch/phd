0920137d2c3ca620a667d8566ac924ad8ba59572
Matthias Retsch <matthias.retsch@monash.edu>
2018-09-19 15:31:21 +1000

    Convective Organisational Potential added.

--- org_metrics.py
+++ org_metrics.py
@@ -10,26 +10,27 @@ start = timeit.default_timer()
 files = "Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_oneday.nc"
 ds_st = xr.open_mfdataset("/Users/mret0001/Data/"+files, chunks={'time': 1000})
 
-stein = ds_st.steiner_echo_classification
-conv = stein.where(stein == 2).fillna(0.)
+stein  = ds_st.steiner_echo_classification
+conv   = stein.where(stein == 2)
+conv_0 = conv.fillna(0.)
 
 props = []
-labeled = np.zeros_like(conv).astype(int)
-for i, scene in enumerate(conv):  # conv has dimension (time, lat, lon). A scene is a lat-lon slice.
+labeled = np.zeros_like(conv_0).astype(int)
+for i, scene in enumerate(conv_0):  # conv has dimension (time, lat, lon). A scene is a lat-lon slice.
     labeled[i, :, :] = skm.label(scene, background=0)
     props.append(skm.regionprops(labeled[i, :, :]))
 
 
-class Partners:
+class Pairs:
 
-    def __init__(self, partnerlist):
-        self.partnerlist = partnerlist
+    def __init__(self, pairlist):
+        self.pairlist = pairlist
 
     def distance(self):
         """The distance in units of pixels between two centroids of cloud-objects found by .regionprops."""
-        # partnerlist is a list of tuples
-        partner1 = list(list(zip(*self.partnerlist))[0])
-        partner2 = list(list(zip(*self.partnerlist))[1])
+        # pairlist is a list of tuples
+        partner1 = list(list(zip(*self.pairlist))[0])
+        partner2 = list(list(zip(*self.pairlist))[1])
 
         dist_x = np.array(list(map(lambda c: c.centroid[1], partner1))) - \
                  np.array(list(map(lambda c: c.centroid[1], partner2)))
@@ -51,15 +52,33 @@ def gen_tuplelist(inlist):
             yield (item1, item2)
 
 
-partner = [Partners(partnerlist=list(gen_tuplelist(cloudlist))) for cloudlist in props]
+all_pairs = [Pairs(pairlist=list(gen_tuplelist(cloudlist))) for cloudlist in props]
+
+test = all_pairs[28].distance()
 
 ########################################################################################################
 
-# For COP [White et al. 2018] just use (props.equivalent_diameter / 2.) to get the radius needed for COP.
-#def cop():
-#    dist = distance()
-#    pass
+def v_potential(pairs):
+    if pairs.pairlist == []: return np.nan
+    partner1 = list(list(zip(*pairs.pairlist))[0])
+    partner2 = list(list(zip(*pairs.pairlist))[1])
+    diameter_1 = np.array(list(map(lambda c: c.equivalent_diameter, partner1)))
+    diameter_2 = np.array(list(map(lambda c: c.equivalent_diameter, partner2)))
 
+    v = np.array(0.5 * diameter_1 + diameter_2 / pairs.distance())
+    return np.sum(v) / len(pairs.pairlist)
+
+
+def cop(allpairs):
+    """The Convective Organisation Potential according to [White et al. 2018]"""
+
+    # get the 'interaction potential', v, for every pair of objects/clouds
+    v = list(map(v_potential, allpairs))
+
+    return v
+
+
+v_pot = cop(all_pairs)
 
 stop = timeit.default_timer()
 print('This script needed {} seconds.'.format(stop-start))
