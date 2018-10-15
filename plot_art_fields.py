bedc9f8744adb33949f3fca442161544d7be787e
Matthias Retsch <matthias.retsch@monash.edu>
2018-10-15 15:06:54 +1100

    Added subplot numbering.

--- plot_art_fields.py
+++ plot_art_fields.py
@@ -15,12 +15,14 @@ grid = AxesGrid(fig, 111,
                 # cbar_pad=0.1
                 )
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
 for i, axis in enumerate(grid):
     # ax.set_axis_off()
     # im = ax.imshow(af.art[i, :, :])
     plot = axis.contourf(af.art.lon, af.art.lat, af.art[i, :, :], levels=[0.5, 1], colors=('k'))
     axis.tick_params(bottom=False, labelbottom=False,
                      left=False, labelleft=False)
+    axis.text(x=129.8, y=-11.05, s=alphabet[i] + ')', verticalalignment='top')
     if i != 4:
         axis.text(x=129.8, y=-13, s='COP = ' + str(cop[i].round(decimals=2).values)
                                     + '\nM1   = ' + str(m1[i].round(decimals=2).values),
