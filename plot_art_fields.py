e502aa1e0c7c8e7973cfb7ae25be3215babfa91a
Matthias Retsch <matthias.retsch@monash.edu>
2018-10-15 13:47:34 +1100

    Plotted the artificial fields figure for the IPR.

--- plot_art_fields.py
+++ plot_art_fields.py
@@ -4,7 +4,9 @@ import numpy as np
 import artificial_fields as af
 import org_metrics as om
 
-fig = plt.figure(figsize=(6, 4))
+cop, m1 = om.run_metrics(artificial=True)
+
+fig = plt.figure(figsize=(9, 6))
 grid = AxesGrid(fig, 111,
                 nrows_ncols=(3, 2),
                 axes_pad=0.05
@@ -19,8 +21,18 @@ for i, axis in enumerate(grid):
     plot = axis.contourf(af.art.lon, af.art.lat, af.art[i, :, :], levels=[0.5, 1], colors=('k'))
     axis.tick_params(bottom=False, labelbottom=False,
                      left=False, labelleft=False)
-    axis.text(x=130, y=-12, s='HELLO')
+    if i != 4:
+        axis.text(x=129.8, y=-13, s='COP = ' + str(cop[i].round(decimals=2).values)
+                                    + '\nM1   = ' + str(m1[i].round(decimals=2).values),
+                  verticalalignment='top')
+    else:
+        axis.text(x=129.8, y=-13, s='COP ='
+                                    + '\nM1   =',
+                  verticalalignment='top')
+        axis.text(x=131.5, y=-13, s=str(cop[i].round(decimals=2).values)
+                                    + '\n' + str(m1[i].round(decimals=2).values),
+                  verticalalignment='top')
+        # axis.tick_params(labelbottom=True, labelleft=True)
 
-cop, m1 = om.run_metrics(artificial=True)
-
+plt.savefig('/Users/mret0001/Desktop/cop_m1_artificial.pdf')
 plt.show()
