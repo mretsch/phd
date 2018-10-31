af6f6413194182d04e325598b7b0ec92b8e55836
Matthias Retsch <matthias.retsch@monash.edu>
2018-10-31 13:47:54 +1100

    Added 2D-hist in Fortran.

--- Plotscripts/plot_2Dhist.py
+++ Plotscripts/plot_2Dhist.py
@@ -2,22 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 import timeit
+import sub as FORTRAN
 
 start = timeit.default_timer()
 
-ds = xr.open_mfdataset(["/Users/mret0001/Data/Analysis/m1_season0910.nc",
-                        "/Users/mret0001/Data/Analysis/cop_season0910.nc"])
+ds = xr.open_mfdataset(["/Users/mret0001/Data/Analysis/m1.nc",
+                        "/Users/mret0001/Data/Analysis/cop.nc"])
 
-# get rid of NaNs.
+# Assign metric to plot and get rid of NaNs.
 x = ds.m1.fillna(-1.)
 y = ds.cop.fillna(-1.)
 
-# Estimate the 2D histogram, range option gets rid of the original NaNs
-nbins = 55
-H, xedges, yedges = np.histogram2d(x, y, bins=nbins, range=[[0, x.max()], [0, y.max()]])
 
-# H needs to transposed for correct plot
-H = H.T
+nbins = 55
+l_fortran = True
+# takes seconds
+if l_fortran:
+    H, xedges, yedges = FORTRAN.histogram_2d(x, y, nbins, [0, x.max()], [0, y.max()])
+
+# takes minutes
+else:
+    # range option gets rid of the original NaNs
+    H, xedges, yedges = np.histogram2d(x, y, bins=nbins, range=[[0, x.max()], [0, y.max()]])
+    # H needs to transposed for correct plot
+    H = H.T
 
 # Mask zeros, hence they do not show in plot
 Hmasked = np.ma.masked_where(H == 0, H)
@@ -31,5 +39,6 @@ cbar = plt.colorbar()
 cbar.ax.set_ylabel('Counts')
 
 fig.show()
+
 stop = timeit.default_timer()
 print('Run Time: ', stop - start)
