323757d1e5b07156cd797c48b3460c45c888443a
Matthias Retsch <matthias.retsch@monash.edu>
2018-11-13 15:48:26 +1100

    Another way of calculating content (overlay) for phase-space.

--- Plotscripts/plot_phase_space.py
+++ Plotscripts/plot_phase_space.py
@@ -15,15 +15,35 @@ overlay = ds.conv_rr_mean
 x_bins = ds_ps.x_series_bins
 y_bins = ds_ps.y_series_bins
 
-# unstack the 2d histogramm and stack the content of the two bin series together . Compare that.
-
 # give the overlay the bin information
-
 overlay.coords['x_bins'] = ('time', x_bins.values)
 overlay.coords['y_bins'] = ('time', y_bins.values)
 
-phase_space_stack = phase_space.stack(z=('x', 'y')) #.load()
+phase_space_stack = phase_space.stack(z=('x', 'y'))
 
+############################################################################
+dict_ind = {}
+for i, binpair in enumerate(phase_space_stack.z):
+    dict_ind[binpair.item()] = i
+
+# give overlay dimension consisting of 3,4,1,... according to x and y bins
+bin_pairs = list(zip(overlay.x_bins.values, overlay.y_bins.values))
+new_coord = [dict_ind[bp] if bp in dict_ind.keys() else np.nan for bp in bin_pairs]
+overlay.coords['bins'] = ('time', new_coord)
+
+# groupby by the new dimension and .mean
+overlay_binned = overlay.groupby('bins').mean()
+
+# order for the new dimension, if not already done by groupby
+
+# assign to phase_stack and unstack it --> overlay in phase_space
+index = overlay_binned.bins.values.astype(int)
+phase_space_stack[index] = overlay_binned.values
+p = phase_space_stack.unstack('z')
+p.T.plot()
+plt.show()
+
+########################################################################
 for i, indices in enumerate(phase_space_stack.z):
     ind = indices.item()
     phase_space_stack[i] = overlay.where((overlay['x_bins'] == ind[0]) & (overlay['y_bins'] == ind[1]), drop=True).mean()
