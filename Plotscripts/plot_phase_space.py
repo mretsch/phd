3bed12c725008f984a87d1ab91e6e87908ace1ae
Matthias Retsch <matthias.retsch@monash.edu>
2018-11-14 13:36:49 +1100

    Compare speed between two ways for phase space.

--- Plotscripts/plot_phase_space.py
+++ Plotscripts/plot_phase_space.py
@@ -4,6 +4,8 @@ import xarray as xr
 import timeit
 from plot_2Dhist import histogram_2d
 
+start = timeit.default_timer()
+
 # no open_mfdataset here, since dask causes runtime-warning in loop below: "invalid value encountered in true_divide"
 ds_ps = xr.open_dataset('/Users/mret0001/Data/Analysis/o_number_area_hist.nc')
 ds = xr.open_dataset('/Users/mret0001/Data/Analysis/conv_rr_mean.nc')
@@ -21,33 +23,35 @@ overlay.coords['y_bins'] = ('time', y_bins.values)
 
 phase_space_stack = phase_space.stack(z=('x', 'y'))
 
-############################################################################
-dict_ind = {}
-for i, binpair in enumerate(phase_space_stack.z):
-    dict_ind[binpair.item()] = i
+slightly_slower = False
+if slightly_slower:
+    dict_ind = {}
+    for i, binpair in enumerate(phase_space_stack.z):
+        dict_ind[binpair.item()] = i
 
-# give overlay dimension consisting of 3,4,1,... according to x and y bins
-bin_pairs = list(zip(overlay.x_bins.values, overlay.y_bins.values))
-new_coord = [dict_ind[bp] if bp in dict_ind.keys() else np.nan for bp in bin_pairs]
-overlay.coords['bins'] = ('time', new_coord)
+    # give overlay dimension consisting of the respective x and y bins
+    bin_pairs = list(zip(overlay.x_bins.values, overlay.y_bins.values))
+    new_coord = [dict_ind[bp] if bp in dict_ind.keys() else np.nan for bp in bin_pairs]
+    overlay.coords['bins'] = ('time', new_coord)
 
-# groupby by the new dimension and .mean
-overlay_binned = overlay.groupby('bins').mean()
+    # groupby by the new dimension and .mean
+    overlay_binned = overlay.groupby('bins').mean()
 
-# order for the new dimension, if not already done by groupby
+    # order for the new dimension, if not already done by groupby
 
-# assign to phase_stack and unstack it --> overlay in phase_space
-index = overlay_binned.bins.values.astype(int)
-phase_space_stack[index] = overlay_binned.values
-p = phase_space_stack.unstack('z')
-p.T.plot()
-plt.show()
+    # assign to phase_stack and unstack it --> overlay in phase_space
+    index = overlay_binned.bins.values.astype(int)
+    phase_space_stack[index] = overlay_binned.values
 
-########################################################################
-for i, indices in enumerate(phase_space_stack.z):
-    ind = indices.item()
-    phase_space_stack[i] = overlay.where((overlay['x_bins'] == ind[0]) & (overlay['y_bins'] == ind[1]), drop=True).mean()
+else:
+    for i, indices in enumerate(phase_space_stack.z):
+        ind = indices.item()
+        phase_space_stack[i] = overlay.where((overlay['x_bins'] == ind[0]) & (overlay['y_bins'] == ind[1])).mean()
 
 ps_overlay = phase_space_stack.unstack('z')
 ps_overlay.T.plot()
 plt.show()
+
+
+stop = timeit.default_timer()
+print('Script needed: {} seconds'.format(stop - start))
