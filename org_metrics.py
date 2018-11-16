77a6eb00948b768ff5b6cc4b97376a21b951136b
Matthias Retsch <matthias.retsch@monash.edu>
2018-11-16 14:16:20 +1100

    Added Iorg metric.

--- org_metrics.py
+++ org_metrics.py
@@ -1,3 +1,4 @@
+import math as m
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,6 +6,7 @@ import timeit
 import skimage.measure as skm
 # from dask.distributed import Client
 import artificial_fields as af
+import scipy as sp
 
 
 class Pairs:
@@ -17,7 +19,7 @@ class Pairs:
             self.partner1, self.partner2 = [], []
 
     def distance(self):
-        """The distance in units of pixels between two centroids of cloud-objects found by .regionprops."""
+        """The distance in units of pixels between two centroids of cloud-objects found by skm.regionprops."""
 
         dist_x = np.array([c.centroid[1] for c in self.partner1]) - \
                  np.array([c.centroid[1] for c in self.partner2])
@@ -47,6 +49,42 @@ def conv_org_pot(pairs):
     diameter_2 = np.array([c.equivalent_diameter for c in pairs.partner2])
     v = np.array(0.5 * (diameter_1 + diameter_2) / pairs.distance())
     return np.sum(v) / len(pairs.pairlist)
+
+
+def I_org(pairs, objects):
+    """I_org according to [Tompkins et al. 2017]"""
+    if not pairs.pairlist:
+        return np.nan
+
+    distances = np.array(pairs.distance())
+    dist_min = []
+
+    faster = True
+    if faster:
+        for cloud in objects:
+            dist_min.append(np.array([distances[i] for i, pair in enumerate(pairs.pairlist) if cloud in pair]).min())
+    else:
+        # maybe faster
+        for cloud in objects:
+            pair_in = np.isin(np.array(pairs.pairlist), cloud, assume_unique=True)
+            pair_one = pair_in[:, 0] + pair_in[:, 1]
+            dist_min.append(distances[pair_one].min())
+
+    # the theoretical Weibull-distribution for n particles
+    u_dist_min, u_dist_min_counts = np.unique(dist_min, return_counts=True)
+    lamda = len(objects) / 9841.  # one radar scan contains 9841 pixels
+    weib_cdf = 1 - np.exp(- lamda * m.pi * u_dist_min**2)
+
+    # the CDF from the actual data
+    data_cdf = np.cumsum(u_dist_min_counts / np.sum(u_dist_min_counts))
+
+    # compute the integral between Weibull CDF and data CDF
+    weib_cdf = np.append(0, weib_cdf   )
+    weib_cdf = np.append(   weib_cdf, 1)
+    data_cdf = np.append(0, data_cdf   )
+    data_cdf = np.append(   data_cdf, 1)
+    cdf_integral = sp.integrate.cumtrapz(data_cdf, weib_cdf, initial=0)
+    return cdf_integral[-1]
 
 
 def metric_1(clouds):
@@ -88,6 +126,7 @@ def run_metrics(artificial=False, file="Steiner/CPOL_STEINER_ECHO_CLASSIFICATION
         conv   = stein.where(stein == 2)
         conv_0 = conv.fillna(0.)
 
+    # splittering does not help with Iorg
     if splitter:
         length = len(ds_st.lat)
         x_y_square = np.arange(length**2).reshape(length, length)
@@ -102,7 +141,12 @@ def run_metrics(artificial=False, file="Steiner/CPOL_STEINER_ECHO_CLASSIFICATION
     all_pairs = [Pairs(pairlist=list(gen_tuplelist(cloudlist))) for cloudlist in props]
 
     # compute the metrics
-    cop = xr.DataArray([conv_org_pot(pairs=p) for p in all_pairs])
+    # conv_org_pot needs 94% of time (tested with splitter=True for data of one day). Because pairs.distance.
+    get_cop = False
+    cop = xr.DataArray([conv_org_pot(pairs=p) for p in all_pairs]) if get_cop else np.nan
+
+    get_Iorg = True
+    Iorg = xr.DataArray([I_org(pairs=all_pairs[i], objects=props[i]) for i in range(len(all_pairs))]) if get_Iorg else np.nan
 
     m1, o_number, o_area, o_area_max = [], [], [], []
     for cloudlist in props:
@@ -119,6 +163,7 @@ def run_metrics(artificial=False, file="Steiner/CPOL_STEINER_ECHO_CLASSIFICATION
     # put together a dataset from the different metrices
     ds_m = xr.Dataset({'cop': cop,
                        'm1': m1,
+                       'Iorg': Iorg,
                        'o_number': o_number,
                        'o_area': o_area,
                        'o_area_max': o_area_max,
@@ -138,7 +183,7 @@ if __name__ == '__main__':
     # compute the metrics
     ds_metric = run_metrics(artificial=False,
                             file="Steiner/CPOL_STEINER_ECHO_CLASSIFICATION_oneday.nc",
-                            splitter=True)
+                            splitter=False)
 
     # a quick histrogram
     # ds_metric.cop.plot.hist(bins=55)
@@ -146,8 +191,10 @@ if __name__ == '__main__':
     # plt.show()
 
     # save metrics as netcdf-files
-    for var in ds_metric.variables:
-        xr.Dataset({var: ds_metric[var]}).to_netcdf('/Users/mret0001/Desktop/'+var+'_new.nc')
+    save = False
+    if save:
+        for var in ds_metric.variables:
+            xr.Dataset({var: ds_metric[var]}).to_netcdf('/Users/mret0001/Desktop/'+var+'_new.nc')
 
     stop = timeit.default_timer()
     print('This script needed {} seconds.'.format(stop-start))
