efd75874f8737b82657c36f92fee2c7a9a91a9d0
Matthias Retsch <matthias.retsch@monash.edu>
2018-12-07 11:16:12 +1100

    Plot shape-COP for artificial fields.

--- Plotscripts/plot_art_fields.py
+++ Plotscripts/plot_art_fields.py
@@ -6,8 +6,8 @@ import org_metrics as om
 
 ds_metric = om.run_metrics(artificial=True)
 
-cop = ds_metric.cop
-cop_mod = ds_metric.cop_mod
+cop = ds_metric.cop_shape
+cop_s = ds_metric.cop_shape
 
 fig = plt.figure(figsize=(9, 8))
 grid = AxesGrid(fig, 111,
@@ -23,21 +23,21 @@ for i, axis in enumerate(grid):
     # ax.set_axis_off()
     # im = ax.imshow(af.art[i, :, :])
 
-    plot = axis.contourf(af.art.lon, af.art.lat, af.art[i, :, :], levels=[0.5, 1], colors='k')
+    plot = axis.contourf(af.art.lon, af.art.lat, af.art[i, :, :], levels=[1.5, 2], colors='k')
 
     axis.tick_params(bottom=False, labelbottom=False,
                      left=False, labelleft=False)
     axis.text(x=129.8, y=-11.05, s=alphabet[i] + ')', verticalalignment='top')
     if i != 4:
         axis.text(x=129.8, y=-13, s='COP = ' + str(cop[i].round(decimals=2).values)
-                                    + '\nCAW = ' + str(cop_mod[i].round(decimals=2).values),
+                                    + '\nSOP = ' + str(cop_s[i].round(decimals=2).values),
                   verticalalignment='top')
     else:
         axis.text(x=129.8, y=-13, s='COP ='
-                                    + '\nCAW =',
+                                    + '\nSOP =',
                   verticalalignment='top')
         axis.text(x=131.5, y=-13, s=str(cop[i].round(decimals=2).values)
-                                    + '\n' + str(cop_mod[i].round(decimals=2).values),
+                                    + '\n' + str(cop_s[i].round(decimals=2).values),
                   verticalalignment='top')
         # axis.tick_params(labelbottom=True, labelleft=True)
 
