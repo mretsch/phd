bc3ecef06e45d740a239fea1403ff53927b5288e
Matthias Retsch <matthias.retsch@monash.edu>
2019-02-22 19:25:20 +1100

    Multi-axis plot script done. Clean-up needed.

--- Plotscripts/plot_multi_yaxis.py
+++ Plotscripts/plot_multi_yaxis.py
@@ -9,9 +9,12 @@ import solarized_colors as colo
 
 start = timeit.default_timer()
 
-var1 = xr.open_dataarray('/Users/mret0001/Data/Analysis/No_Boundary/rom.nc').sel({'time':slice('2009-11-30','2009-12-02')})
-var2 = xr.open_dataarray('/Users/mret0001/Data/Analysis/With_Boundary/conv_intensity.nc').sel({'time':slice('2009-11-30','2009-12-02')})
-var3 = xr.open_dataarray('/Users/mret0001/Data/Analysis/With_Boundary/conv_rr_mean.nc').sel({'time':slice('2009-11-30','2009-12-02')})  # * 100
+plt.rc('font'  , size=12)
+plt.rc('legend', fontsize=12)
+
+var1 = xr.open_dataarray('/Users/mret0001/Data/Analysis/No_Boundary/rom.nc')#.sel({'time':slice('2009-11-30','2009-12-02')})
+var2 = xr.open_dataarray('/Users/mret0001/Data/Analysis/With_Boundary/conv_intensity.nc')#.sel({'time':slice('2009-11-30','2009-12-02')})
+var3 = xr.open_dataarray('/Users/mret0001/Data/Analysis/With_Boundary/conv_rr_mean.nc')#.sel({'time':slice('2009-11-30','2009-12-02')})  # * 100
 
 
 def identity(x):
@@ -86,43 +89,69 @@ ax_r2.spines["right"].set_position(("axes", 1.11))
 #ax_r2.spines["right"].set_visible(True)
 
 # one-element unpacking
-p1, = ax_host.plot(var_actual[0].time, var_actual[0], "k",          lw=2, label="ROM", alpha=0.)
-p2, =   ax_r1.plot(var_actual[1].time, var_actual[1], colo.orange,  lw=1, label="Conv. rain intensity")
-p3, =   ax_r2.plot(var_actual[2].time, var_actual[2], colo.violet,  lw=1, label="Conv. mean rain")
-p4, =   ax_r3.plot(var_actual[0].time, var_actual[0], "k",          lw=2, label="ROM")
+p0, = ax_host.plot(var_actual[0].time, var_actual[0], "k",          lw=2, label="ROM", alpha=0.)
+p1, =   ax_r1.plot(var_actual[1].time, var_actual[1], colo.orange,  lw=1, label="Conv. rain intensity")
+p2, =   ax_r2.plot(var_actual[2].time, var_actual[2], colo.violet,  lw=1, label="Conv. mean rain")
+p3, =   ax_r3.plot(var_actual[0].time, var_actual[0], "k",          lw=2, label="ROM")
 
 ax_host.set_xlim('2019-01-07T00:00:00', '2019-01-07T23:50:00')
-#ax_host.set_ylim(0, 2)
-#ax_r1.set_ylim(0, 17)
-#ax_r2.set_ylim(0, 0.3)
+ax_r3.  set_xlim('2019-01-07T00:00:00', '2019-01-07T23:50:00')
+ax_host.set_ylim(8, 18)
+ax_r3.  set_ylim(8, 18)
+ax_r1.set_ylim(bottom=11.5)
+ax_r2.set_ylim(bottom=0.08)
 
 ax_host.set_xlabel("Time of day [hour]")
 ax_host.set_ylabel("ROM [1]")
-ax_r1.set_ylabel("Conv. rain intensity [mm/hour]")
-ax_r2.set_ylabel("Conv. mean rain [mm/hour]")
+ax_r1.  set_ylabel("Conv. rain intensity [mm/hour]")
+ax_r2.  set_ylabel("Conv. mean rain [mm/hour]")
 
-ax_host.yaxis.label.set_color(p1.get_color())
-ax_r1.yaxis.label.set_color(p2.get_color())
-ax_r2.yaxis.label.set_color(p3.get_color())
+#ax_host.yaxis.label.set_color(p0.get_color())
+#ax_r1.yaxis.label.set_color(p1.get_color())
+#ax_r2.yaxis.label.set_color(p2.get_color())
+
+#ax_r3.spines["right"].set_color('blue')
+#ax_r3.spines["right"].set_visible(False)
+#ax_r3.yaxis.set_ticks_position('none')
+#make_patch_spines_invisible(ax_r3)
+#ax_r3.yaxis.set_alpha(0.)
+#ax_r3.yaxis.remove()
+
+# no spines, labels, ticks, ticklabels (at top, bottom, left, right) whatsoever
+ax_r3.set_axis_off()
 
 ax_host.xaxis.set_major_formatter(hourFmt)
 
+for ax in [ax_host, ax_r1, ax_r2]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    # this only tells if ticks are at top, bottom, left, right spine!
+    #ax.xaxis.set_ticks_position('none')
+    ax.tick_params(axis='x', bottom=False)
+    # remove one single tick
+    xticks = ax.xaxis.get_major_ticks()
+    xticks[0].label1.set_visible(False)
+
+# direction brings ticklabels closer, the boolean turns the ticks off
+ax_host.tick_params(axis='y', direction='in', left=False)
+ax_r1.tick_params(axis='y', direction='in', right=False)
+ax_r2.tick_params(axis='y', direction='in', right=False)
+
 #tkw = dict(size=4, width=1.5)
-tkw = dict()
 #ax_host.tick_params(axis='y', colors=p1.get_color(), **tkw)
 #ax_r1.  tick_params(axis='y', colors=p2.get_color(), **tkw)
 #ax_r2.  tick_params(axis='y', colors=p3.get_color(), **tkw)
 #ax_host.tick_params(axis='x', **tkw)
 
-lines = [p4, p2, p3]
-
-ax_host.legend(lines, [l.get_label() for l in lines])
+plots = [p3, p1, p2]
+ax_host.legend(plots, [p.get_label() for p in plots], framealpha=1.)
 
 ax_host.grid(which='both', axis='x', **dict(linestyle='--'))
 
 save = False
 if save:
-    plt.savefig(home+'/Desktop/plot.pdf')
+    # bbox_inches captures the right portion of the plot
+    plt.savefig(home+'/Desktop/plot.pdf', bbox_inches='tight', transparent=True)
 else:
     plt.show()
 
