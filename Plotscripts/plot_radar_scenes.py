25ee3374af5e106587963eb543faad3cfdbc3ad5
Matthias Retsch <matthias.retsch@monash.edu>
2019-03-20 12:21:02 +1100

    Final plots for radar scenes.

--- Plotscripts/plot_radar_scenes.py
+++ Plotscripts/plot_radar_scenes.py
@@ -1,14 +1,20 @@
 from os.path import expanduser
 home = expanduser("~")
 import xarray as xr
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.colors import ListedColormap
 import timeit
 
 start = timeit.default_timer()
 
+plt.rc('font'  , size=12)
+plt.rc('legend', fontsize=12)
+
 try:
-    metric_1   = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/sic.nc')
-    metric_2   = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/rom.nc')
+    metric_1   = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/rom.nc')
+    metric_2   = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/sic.nc')
     ds_steiner = xr.open_mfdataset(home+'/Data/Steiner/*season*', chunks=40)
 except FileNotFoundError:
     metric_1 = xr.open_dataarray(home+'/Google Drive File Stream/My Drive/Data_Analysis/rom.nc')#\
@@ -32,18 +38,45 @@ else:
 
     idx_median = int(len(m_sort) / 2)
     idx_mean = int(abs(m_sort - m_sort.mean()).argmin())
+    idx_33percent = round(0.33 * len(m_sort))
+    idx_66percent = round(0.66 * len(m_sort))
     idx_90percent = round(0.9 * len(m_sort))
     idx_99percent = round(0.99 * len(m_sort))
     idx_value = abs(m_sort - 10.8).argmin().item()
-    metric1_select = m_sort[idx_99percent-10:idx_99percent+10]  #[idx_value-10:idx_value+10]  # [idx_median-10:idx_median+10]  #[:20]  #[idx_mean-10:idx_mean+10]  #[-20:]  #[-60:-40] #
+    idx_my_select = [0, idx_33percent-1, idx_66percent-1, -1]
+    metric1_select = m_sort[idx_my_select]  #[idx_99percent-10:idx_99percent+10]  #[idx_value-10:idx_value+10]  # [idx_median-10:idx_median+10]  #[:20]  #[idx_mean-10:idx_mean+10]  #[-20:]  #[-60:-40] #
 
     steiner_select = steiner.loc[metric1_select.time]
     metric2_select = metric_2.loc[metric1_select.time]
 
-# aspect is a hack based on measuring pixels on my screen. aspect=1 for a square plot did not work as intended.
-# p = steiner_select.plot(col='time', col_wrap=4, add_colorbar=False, aspect=1, size=4)
-p = steiner_select.plot(col='time', col_wrap=4, add_colorbar=False, aspect=1 - abs(1 - 679./740), size=4)
+# plotting
 
+# create own colormap. matplotlib.org/tutorials/colors/colormap-manipulation.html
+vals = np.ones((256, 4))
+for i in range(3):
+    vals[   :100, i] = 1.0  # white
+    vals[100:200, i] = 0.95  # light grey
+    vals[200:   , i] = 0.0  # black
+newcmp = ListedColormap(vals)
+# aspect is a hack based on measuring pixels on my screen. aspect=1 for a square plot did not work as intended.
+p = steiner_select.plot(col='time', col_wrap=4, add_colorbar=False, aspect=450./558, size=4,
+                        cmap=newcmp)
+p.axes.flat[0].axes.set_ylabel('Latitude [$^\circ$S]')
+p.axes.flat[0].axes.set_yticklabels(labels=['14.0', '13.5', '13.0', '12.5', '12.0', '11.5', '11.0', '10.5'])
+for ax in p.axes.flat:
+    ax.set_title('')
+    ax.axes.set_xlabel('Longitude [$^\circ$E]')
+
+    # stackoverflow.com/questions/18390068/hatch-a-nan-region-in-a-contourplot-in-matplotlib#18403408
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    xy = (xmin, ymin)
+    width = xmax - xmin
+    height = ymax - ymin
+    patch = patches.Rectangle(xy, width, height, hatch='xxxx', color='lightgrey', fill=None, zorder=-10)
+    ax.add_patch(patch)
+
+# Print some information on plots
 percentiles = True
 as_coordinate = True
 if percentiles:
@@ -57,17 +90,17 @@ if percentiles:
 print1 = metric1_select
 print2 = metric2_select
 
-for i, ax in enumerate(p.axes.flat):
-    ax.annotate('SIC: {:5.1f}\n'
-                'ROM: {:5.1f}'.format(print1[i].item(),
-                                      print2[i].item()), (131.78, -11.2), color='blue')
-    if percentiles:
-        ax.annotate('SIC: {:3.0f}%\n'
-                    'ROM: {:3.0f}%'.format(perct1[i].item(),
-                                           perct2[i].item()), (131.78, -13.5), color='blue')
+# for i, ax in enumerate(p.axes.flat):
+#     ax.annotate('ROM: {:5.1f}\n'
+#                 'SIC: {:5.1f}'.format(print1[i].item(),
+#                                       print2[i].item()), (131.78, -11.2), color='blue')
+#     if percentiles:
+#         ax.annotate('ROM: {:3.0f}%\n'
+#                     'SIC: {:3.0f}%'.format(perct1[i].item(),
+#                                            perct2[i].item()), (131.78, -13.5), color='blue')
 
-plt.savefig(home+'/Desktop/radar_scenes1.pdf')
-#plt.show()
+#plt.savefig(home+'/Desktop/radar_scenes1.pdf', transparent=True, bbox_inches='tight')
+plt.show()
 
 # Have all scenes separately as a pdf to create a gif
 # for i, scene in enumerate(steiner_select):
