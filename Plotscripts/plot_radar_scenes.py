215111a3dab28b16e8bd70a153aa19feb347a877
Matthias Retsch <matthias.retsch@monash.edu>
2020-01-17 11:53:49 +1100

    Radar plots. (cherry picked from commit ce2bdeaef056b826f158cc854d957f611210e048)

--- Plotscripts/plot_radar_scenes.py
+++ Plotscripts/plot_radar_scenes.py
@@ -5,8 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.colors import ListedColormap
-import cartopy.crs as ccrs
-import cartopy.io as cio
 from datetime import datetime
 import timeit
 
@@ -22,18 +20,17 @@ metric_4   = metric_1 # xr.open_dataarray(home+'/Data/Analysis/No_Boundary/scai.
 ds_steiner = xr.open_mfdataset(home+'/Data/Steiner/*season*', chunks=40)
 
 timeselect = True
-contiguous = True
+contiguous = False
 if timeselect:
-    start_date = '2015-11-10T03:00' # '2005-03-11T15:10:00' # '2017-03-30T14:50:00' # '2009-12-07T09:10:00'
-    end_date   = '2015-11-10T06:10' # '2005-03-11T20:50:00' # '2017-03-30T18:00:00' # '2009-12-07T12:20:00'
+    start_date = '2015-11-10T03:00:00' # '2006-01-09T09:00' # '2017-03-30T14:50:00' # '2009-12-07T09:10:00'
+    end_date   = '2015-11-10T06:10:00' # '2006-01-09T14:50' # '2017-03-30T18:00:00' # '2009-12-07T12:20:00'
     if contiguous:
         times = slice(start_date, end_date)
     else:
         # the order in next lines is not relevant, intersection is sorted along time coordinate
-        # times = ds_steiner.indexes['time'].intersection(['2009-12-22T06:20:00', '2009-12-07T11:00:00'])
-        times = [np.datetime64('2009-12-22T06:20:00'), np.datetime64('2009-12-07T11:00:00')]
-        # times = [np.datetime64('2013-02-24T08:40:00'), np.datetime64('2016-11-19T14:50:00'),
-        #          np.datetime64('2011-03-15T06:10:00'), np.datetime64('2017-02-27T13:00:00')]
+        # times = ds_steiner.indexes['time'].intersection(['2009-12-04T10:30:00', '2009-12-07T11:00:00'])
+        times = [np.datetime64('2013-02-24T08:40:00'), np.datetime64('2016-11-19T14:50:00'),
+                 np.datetime64('2011-03-15T06:10:00'), np.datetime64('2017-02-27T13:00:00')]
 
     steiner_select = ds_steiner.steiner_echo_classification.sel(time=times)
     time_select    = ds_steiner.time.sel(time=times)
@@ -59,9 +56,7 @@ else:
     idx_99percent = round(0.99 * len(m_sort))
     idx_value     = abs(m_sort - 10.8).argmin().item()
     idx_my_select = [2, idx_25percent+1, idx_median, idx_75percent+2, -2]
-    metric1_select = m_sort[idx_my_select]  #[idx_99percent-10:idx_99percent+10]  #[idx_value-10:idx_value+10]
-                                            # [idx_median-10:idx_median+10]  #[:20]
-                                            #[idx_mean-10:idx_mean+10]  #[-20:]  #[-60:-40] #
+    metric1_select = m_sort[idx_my_select]  #[idx_99percent-10:idx_99percent+10]  #[idx_value-10:idx_value+10]  # [idx_median-10:idx_median+10]  #[:20]  #[idx_mean-10:idx_mean+10]  #[-20:]  #[-60:-40] #
 
     steiner_select = steiner.loc        [metric1_select.time]
     time_select    = ds_steiner.time.loc[metric1_select.time]
@@ -75,83 +70,66 @@ else:
 radar_mask = xr.where(steiner_select[0].isnull(), 1, 0)
 
 # create own colormap. matplotlib.org/tutorials/colors/colormap-manipulation.html
-vals = np.ones((256, 4)) # 256 different colors in this colormap
+vals = np.ones((256, 4))
 for i in range(3):
-    vals[   :100, i] = 1.0        # white
-    vals[100:200, i] = 1.0        # white
-    vals[200:   , 0] =   0 / 255  # blue, r
-    vals[200:   , 1] = 162 / 255  # blue, g
-    vals[200:   , 2] = 255 / 255  # blue, b
-vals[:200, 3] = 0. # transparent white
-transparent_blue = ListedColormap(vals)
-
-# another colormap
-vals = np.ones((256, 4)) # 256 different colors in this colormap
-for i in range(3):
-    vals[   :100, i] = 1.0  # white
-    vals[100:200, i] = 1.0  # white
-    vals[200:   , 0] = 1.0  # white, r
-    vals[200:   , 1] = 1.0  # white, g
-    vals[200:   , 2] = 1.0  # white, b
-vals[:200, 3] = 0. # transparent white
-transparent_white = ListedColormap(vals)
+    vals[   :100, i] = 1.0   # white
+    # vals[100:200, i] = 0.95  # light grey
+    vals[100:200, i] = 1.0   # white
+    # vals[200:   , i] = 0.0   # black
+    vals[200:, 0] =   0 / 255  # blue, r
+    vals[200:, 1] = 162 / 255  # blue, g
+    vals[200:, 2] = 255 / 255  # blue, b
+newcmp = ListedColormap(vals)
 
 # letters to print on plots
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
 alphabet_numbered = ['a$_1$', 'b$_1$', 'a$_2$', 'b$_2$', 'c$_1$', 'c$_2$']
+# title_numbers = ['ROME=157 km$^2$, R$_\mathrm{NI}$=130 km$^2$', 'ROME=127 km$^2$, R$_\mathrm{NI}$=126 km$^2$',
+#                  'ROME=191 km$^2$, R$_\mathrm{min}$=129 km$^2$', 'ROME=250 km$^2$, R$_\mathrm{min}$=130 km$^2$']
 title_numbers = ['$\mathcal{R}_\mathrm{NI}$=130, ROME=157', '$\mathcal{R}_\mathrm{min}$=129, ROME=191',
                  '$\mathcal{R}_\mathrm{NI}$=126, ROME=127', '$\mathcal{R}_\mathrm{min}$=130, ROME=250']
 darwin_time = np.timedelta64(570, 'm')  # UTC + 9.5 hours
 
-n_per_row = 4
+n_per_row = 2
+# aspect is a hack based on measuring pixels on my screen. aspect=1 for a square plot did not work as intended.
+if n_per_row == 2:
+    # fontsize = 16
+    # p = steiner_select.plot(col='time', col_wrap=n_per_row, add_colorbar=False, aspect=700./880., size=4, cmap=newcmp)
+    fontsize = 19
+    p = steiner_select.plot(col='time', col_wrap=n_per_row, add_colorbar=False, aspect=260./300., size=4, cmap=newcmp)
+if n_per_row == 5:
+    fontsize = 19
+    p = steiner_select.plot(col='time', col_wrap=n_per_row, add_colorbar=False, aspect=614./754, size=4, cmap=newcmp)
+    # fontsize = 22
+    # p = steiner_select.plot(col='time', col_wrap=n_per_row, add_colorbar=False, aspect=684./754, size=4, cmap=newcmp)
+if n_per_row == 4:
+    fontsize = 19
+    p = steiner_select.plot(col='time', col_wrap=n_per_row, add_colorbar=False, aspect=684./754, size=4, cmap=newcmp)
 
-fontsize = 19
-fig, axes_list = plt.subplots(nrows=5, ncols=4, figsize=(1.4* 13.4, 1.4* 16),
-                              subplot_kw=dict(projection=ccrs.PlateCarree()))
-
-plt.subplots_adjust(hspace = 0.05, wspace=0.01)
-
-for i, ax in enumerate(axes_list.flatten()):
+for i, ax in enumerate(p.axes.flat):
     plt.sca(ax)  # sets the current axis ('plot') to draw to
+    radar_mask.plot.contour(colors='k', linewidths=0.5, levels=1)
 
-    # Out of radar range, paint over in white. Inside leave transparent.
-    ax.pcolormesh(ds_steiner.lon, ds_steiner.lat, radar_mask, zorder=2000, cmap=transparent_white)
-
-    # Paint a line at the radar range boundary
-    radar_mask.plot.contour(colors='k', linewidths=0.5, levels=1, zorder=5000)
-
-    # draw the coast-line
-    lons = ds_steiner.lon[:]
-    lats = ds_steiner.lat[:]
-    width = 0  # degrees east,west,north,south
-    extent = [min(lons) - width, max(lons) + width, min(lats) - width, max(lats) + width]  # Darwin [lon,lon,lat,lat]
-    # set_extent speeds up the drawing of all the land polygons
-    # ax.set_extent(extent)
-
-    shp = cio.shapereader.Reader(home+'/Data/OpenStreetMaps/land-polygons-complete-4326/Australia_polygons')
-    for record, geometry in zip(shp.records(), shp.geometries()):
-        ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='white', edgecolor='black')
-
-    # Plot the actual convective objects in blue and leave the rest transparent, so coastlines are visible
-    ax.pcolormesh(lons, lats, steiner_select[i], zorder=1000, cmap=transparent_blue)
-                   # color=[(0.1, 0.8, 0.5, 0.2), (0.9, 0.1, 0.5, 0.2)])
-
-    # ax.set_axis_off()
-
+    #ax.set_axis_off()
     ax.set_title('')
     ax.axes.set_xlabel('')
     ax.axes.set_ylabel('')
-    # ax.get_xaxis().set_visible(False)
-    # ax.get_yaxis().set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
-    # ax.xaxis.set_ticks_position('none')
-    # ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
 
-    ax.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    ax.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+    #    # hatch the nan-region
+    #    # stackoverflow.com/questions/18390068/hatch-a-nan-region-in-a-contourplot-in-matplotlib#18403408
+    #    xmin, xmax = ax.get_xlim()
+    #    ymin, ymax = ax.get_ylim()
+    #    xy = (xmin, ymin)
+    #    width = xmax - xmin
+    #    height = ymax - ymin
+    #    patch = patches.Rectangle(xy, width, height, hatch='xxxx', color='lightgrey', fill=None, zorder=-10)
+    #    ax.add_patch(patch)
 
     if timeselect and not contiguous:
         # ax.text(x=129.8, y=-11.0, s=str(alphabet_numbered[i]) + ')', verticalalignment='top', fontdict={'fontsize': fontsize})
@@ -159,10 +137,10 @@ for i, ax in enumerate(axes_list.flatten()):
         ax.set_title(str(alphabet_numbered[i]) + ')   ' + title_numbers[i])
     if contiguous:
         ax.text(x=131.75, y=-11.05, s=str((time_select[i] + darwin_time).values)[11:16]+' h', verticalalignment='top'
-                , fontdict={'fontsize': 16}, zorder=6000)
+                , fontdict={'fontsize': 16})
 
 # all the bottom plots
-for ax in axes_list.flatten()[-n_per_row:]:
+for ax in p.axes.flat[-n_per_row:]:
     ax.spines['bottom'].set_visible(True)
     ax.xaxis.set_ticks_position('bottom')
     ax.axes.set_xlabel('Longitude [$^\circ$E]', fontdict={'fontsize': fontsize})
@@ -170,13 +148,11 @@ for ax in axes_list.flatten()[-n_per_row:]:
     ax.axes.set_xticklabels(labels=['130', '', '131', '', '132'], fontdict={'fontsize': fontsize})
 
 # all the left plots
-for i in np.arange(0, len(axes_list.flatten()), n_per_row):
-    axes_list.flatten()[i].spines['left'].set_visible(True)
-    axes_list.flatten()[i].yaxis.set_ticks_position('left')
-    axes_list.flatten()[i].axes.set_ylabel('Latitude [$^\circ$S]', fontdict={'fontsize': fontsize})
-    axes_list.flatten()[i].axes.set_yticks([-11, -11.5, -12, -12.5, -13, -13.5])
-    # axes_list.flatten()[i].axes.set_yticklabels(labels=['', '13', '', '12', '', '11'], fontdict={'fontsize': fontsize})
-    axes_list.flatten()[i].axes.set_yticklabels(labels=['11', '', '12', '', '13', ''], fontdict={'fontsize': fontsize})
+for i in np.arange(0, len(p.axes.flat), n_per_row):
+    p.axes.flat[i].spines['left'].set_visible(True)
+    p.axes.flat[i].yaxis.set_ticks_position('left')
+    p.axes.flat[i].axes.set_ylabel('Latitude [$^\circ$S]', fontdict={'fontsize': fontsize})
+    p.axes.flat[i].axes.set_yticklabels(labels=['xxx', '', '13', '', '12', '', '11'], fontdict={'fontsize': fontsize})
 
 # Print some information on plots
 percentiles = False
@@ -194,7 +170,7 @@ print2 = metric2_select
 
 print_numbers = False
 if print_numbers:
-    for i, ax in enumerate(axes_list.flatten()):
+    for i, ax in enumerate(p.axes.flat):
         ax.annotate('ROME: {:5.1f}'.format(print1[i].item()), (131.78, -11.2), color='blue')
         if percentiles:
             ax.annotate('ROM: {:3.0f}%\n'
