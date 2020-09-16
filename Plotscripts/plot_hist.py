3f2c77fe88662fd05f120c0dc383242670b4cffd
Matthias Retsch <matthias.retsch@monash.edu>
2020-09-16 17:30:12 +0200

    LS variables in radar phase space.

--- Plotscripts/plot_hist.py
+++ Plotscripts/plot_hist.py
@@ -138,7 +138,7 @@ def histogram_2d(x_series, y_series, nbins=None, x_label='', y_label='', cbar_la
         H, xbinseries, ybinseries = FORTRAN.histogram_2d(xseries=x_series, yseries=y_series,
                                                          xedges=x_edges, yedges=y_edges,
                                                          l_density=False,
-                                                         l_cut_off=True, cut_off=10)
+                                                         l_cut_off=True, cut_off=9)
         xbinseries[xbinseries == -1.] = np.nan
         ybinseries[ybinseries == -1.] = np.nan
         # the cut-away part
@@ -216,13 +216,13 @@ if __name__ == '__main__':
             ls_sub = ls.where(ls.hour.isin([6]), drop=True)
             ls = ls_sub
 
-        var1 = ls.omega.sel(lev=515)
-        var2 = ls.RH.sel(lev=515)
-        # var1 = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/o_area_kilometres.nc')
-        # var2 = xr.open_dataarray(home+'/Data/Analysis/No_Boundary/o_number.nc')
+        # var1 = ls.omega.sel(lev=515)
+        # var2 = ls.RH.sel(lev=515)
+        var1 = xr.open_dataarray(home+'/Documents/Data/Analysis/No_Boundary/AllSeasons/o_area.nc') * 6.25
+        var2 = xr.open_dataarray(home+'/Documents/Data/Analysis/No_Boundary/AllSeasons/o_number.nc')
         # var1 = var1.where(var2)
 
-        l_no_singlepixel = False
+        l_no_singlepixel = True
         if l_no_singlepixel:
             # don't take scenes where convection is 1 pixel large only
             # var1 = var1[var1 != 6.25]
@@ -232,9 +232,9 @@ if __name__ == '__main__':
             # var1 = var1.where(var2)
             var2 = var2.where(var1)
 
-        fig_h_2d, h_2d = histogram_2d(var1, var2,  nbins=17,
-                                      x_label= var1.long_name+' ['+var1.units+']', #'Object mean area [km$^2$]', #
-                                      y_label= var2.long_name+' ['+var2.units+']', # 'Number of objects [1]', #
+        fig_h_2d, h_2d = histogram_2d(var1, var2,  nbins=35,
+                                      x_label= 'Object mean area [km$^2$]', # var1.long_name+' ['+var1.units+']', #
+                                      y_label= 'Number of objects [1]', #     var2.long_name+' ['+var2.units+']', #
                                       cbar_label='%', # '[% dx$^{{-1}}$ dy$^{{-1}}$]')
                                       l_same_axis_length=False)
         fig_h_2d.show()
--- Plotscripts/plot_phase_space.py
+++ Plotscripts/plot_phase_space.py
@@ -10,9 +10,11 @@ home = expanduser("~")
 start = timeit.default_timer()
 
 # no open_mfdataset here, since dask causes runtime-warning in loop below: "invalid value encountered in true_divide"
-# ds_ps = xr.open_dataset(home+'/Documents/Plots/Phase_Space/515rh_515omega_hist.nc')
-ds_ps = xr.open_dataset(home+'/Desktop/hist.nc')
-ds    = xr.open_dataset(home+'/Documents/Data/Analysis/No_Boundary/AllSeasons/rom_km_avg6h_nanzero.nc')
+ds_ps = xr.open_dataset(home+'/Documents/Plots/2D_Histograms/area_number_hist_ge9.nc')
+ls    = xr.open_dataset(home+'/Documents/Data/LargeScaleState/' +
+                        'CPOL_large-scale_forcing_cape990hPa_cin990hPa_rh_shear_dcape.nc')
+ds    = ls.RH.sel(lev=515).resample(time='10min').interpolate('linear')
+# ds    = xr.open_dataset(home+'/Documents/Data/Analysis/No_Boundary/AllSeasons/rom_km_avg6h_nanzero.nc')
 # ds    = xr.open_dataset(home+'/Documents/Data/Analysis/With_Boundary/conv_intensity.nc')
 
 subselect = True
@@ -26,15 +28,16 @@ if subselect:
     # subselect on the times given in the histogram data
     l_histogram = True
     if l_histogram:
-        ds_sub = ds.sel(time=ds_ps.time)
-    ds = ds_sub
+        # ds_sub = ds.sel(time=ds_ps.time)
+        ds_sub = ds[ds.time.isin(ds_ps.time)]
+    ds    = ds_sub
 
 phase_space = ds_ps.hist_2D
-overlay = ds.rom_kilometres #conv_intensity #rom_kilometres #conv_intensity # div.sel(lev=845) # RH.sel(lev=990) # .where(ds.cop_mod < 60.)
+overlay = ds # .RH.sel(lev=515) #rom_kilometres #conv_intensity #conv_intensity # div.sel(lev=845) # .where(ds.cop_mod < 60.)
 
 # give the overlay time series information about the placements of the bins for each time step
-overlay.coords['x_bins'] = ('time', ds_ps.x_series_bins.values)
-overlay.coords['y_bins'] = ('time', ds_ps.y_series_bins.values)
+overlay.coords['x_bins'] = ('time', ds_ps.x_series_bins[ds_ps.time.isin(ds_sub.time)].values)
+overlay.coords['y_bins'] = ('time', ds_ps.y_series_bins[ds_ps.time.isin(ds_sub.time)].values)
 
 phase_space_stack = phase_space.stack(z=('x', 'y'))
 
@@ -44,9 +47,10 @@ phase_space_stack[:] = FORTRAN.phasespace(indices1=ind_1,
                                           overlay=overlay,
                                           overlay_x=overlay['x_bins'],
                                           overlay_y=overlay['y_bins'],
-                                          l_probability=True,
+                                          l_probability=False,
                                           upper_bound=10000.,
-                                          lower_bound=overlay[overlay.percentile>0.9].min())
+                                          # lower_bound=overlay[overlay.percentile > 0.9].min(),
+                                          lower_bound=0.)
 
 # set NaNs to the special values set in the Fortran-routine
 phase_space_stack = phase_space_stack.where((phase_space_stack < -1e10) ^ (-9999999998.0 < phase_space_stack))
@@ -58,10 +62,13 @@ plt.rc('font'  , size=18)
 plt.rc('legend', fontsize=18)
 # plt.style.use('dark_background')
 
-the_plot = ps_overlay.T.plot(cmap='rainbow') # (robust=True)  # (cmap='inferno')  # (cmap='tab20c')
-plt.xlabel('$\omega$ at 515 hPa [hPa/h]')
-plt.ylabel('RH at 515 hPa [1]')
-the_plot.colorbar.set_label('Probability of highest ROME decile [1]')
+the_plot = ps_overlay.T.plot(cmap='BrBG') # (robust=True)  # (cmap='inferno')  # (cmap='tab20c')
+# plt.xlabel('$\omega$ at 515 hPa [hPa/h]')
+# plt.ylabel('RH at 515 hPa [1]')
+# the_plot.colorbar.set_label('Probability of highest ROME decile [1]')
+plt.xlabel('Object mean area [km$^2$]')
+plt.ylabel('Number of objects [1]')
+the_plot.colorbar.set_label('RH at 515 hPa [1]')
 
 save = True
 if save:
