7d48885b90260c006a5270272ae9cd65c575a4b7
Matthias Retsch <matthias.retsch@monash.edu>
2021-04-27 09:52:23 +0200

    Land-mask for tropics.

--- Plotscripts/plot_maps_simulation.py
+++ Plotscripts/plot_maps_simulation.py
@@ -3,44 +3,11 @@ home = expanduser("~")
 import timeit
 import numpy as np
 import xarray as xr
-
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import cartopy.crs as ccrs
 import cartopy.io as cio
 from Plotscripts.colors_solarized import sol
-
-start = timeit.default_timer()
-plt.rc('font'  , size=20)
-
-rome = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rome_14mmhour.nc')
-rh = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rh500.nc')
-
-rome_avg = rome.sum(dim='time') / len(rome.time)
-# rome_avg = rome.max(dim='time')
-l_rome_p90 = (rome > np.nanpercentile(rome, q=90))
-rh_p90 = rh.where(l_rome_p90, other=np.nan)
-rhlow_p90 = (rh_p90 < 40)
-
-rome_p90_count = (rome > np.nanpercentile(rome, q=90)).sum(dim='time')
-# highest count of rome above 90th percentile
-rome_southofindia = rome.sel(lat=-8.064, lon=80.18, method='nearest')
-
-area = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/o_area_14mmhour.nc')
-area_avg = area.sum(dim='time') / len(area.time)
-
-number = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/o_number_14mmhour.nc')
-
-rome_ni = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rome_ni_14mmhour.nc')
-delta_size = rome_ni - area
-delta_prox = rome - rome_ni
-
-land_sea = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/land_sea_avg.nc')
-coast_mask = (0.2<land_sea) & (land_sea<0.8)
-coast_mask['lat'] = rome['lat']
-coast_mask['lon'] = rome['lon']
-rome_coast = rome.where(coast_mask, other=np.nan)
-
 
 
 def make_map(projection=ccrs.PlateCarree()):
@@ -52,30 +19,77 @@ def make_map(projection=ccrs.PlateCarree()):
     #gl.yformatter = LATITUDE_FORMATTER
     return fig, ax
 
-# ratio = rome / area
-# map_to_plot = (delta_prox.sum(dim='time') / delta_size.sum(dim='time')) # rome.mean(dim='time')# ratio.sum(dim='time') / len(ratio.time)
-# map_to_plot = delta_prox.sum(dim='time') / len(rome.time)
-# map_to_plot = rhlow_p90.sum(dim='time').sel(lat=slice(-20, -10), lon=slice(120, 150))
-# map_to_plot = (rome > np.nanpercentile(rome, q=90)).sum(dim='time')#.sel(lat=slice(-15, 0), lon=slice(70, 90))
-map_to_plot = rhlow_p90.sum(dim='time').where(coast_mask, other=np.nan).sel(lat=slice(-20, -10), lon=slice(120, 134))
 
-fig, ax = make_map(projection=ccrs.PlateCarree())
+start = timeit.default_timer()
+plt.rc('font'  , size=20)
+
+# rome = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rome_14mmhour.nc')
+# rh = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rh500.nc')
+precip = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/pr_avg_0201to301.nc') * 3600
+
+# rome_avg = rome.sum(dim='time') / len(rome.time)
+# rome_avg = rome.max(dim='time')
+# l_rome_p90 = (rome > np.nanpercentile(rome, q=90))
+# rh_p90 = rh.where(l_rome_p90, other=np.nan)
+# rhlow_p90 = (rh_p90 < 40)
+
+# rome_p90_count = (rome > np.nanpercentile(rome, q=90)).sum(dim='time')
+# highest count of rome above 90th percentile
+# rome_southofindia = rome.sel(lat=-8.064, lon=80.18, method='nearest')
+
+# area = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/o_area_14mmhour.nc')
+# area_avg = area.sum(dim='time') / len(area.time)
+
+# number = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/o_number_14mmhour.nc')
+
+# rome_ni = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/rome_ni_14mmhour.nc')
+# delta_size = rome_ni - area
+# delta_prox = rome - rome_ni
+
+l_pick_surface = True
+if l_pick_surface:
+    land_sea = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/land_sea_avg.nc')
+    # coast_mask = (0.2 < land_sea) & (land_sea < 0.8) # coasts
+    # coast_mask =  land_sea < 0.2                     # ocean
+    # coast_mask['lat'] = rome['lat']
+    # coast_mask['lon'] = rome['lon']
+    # rome_coast = rome.where(coast_mask, other=np.nan)
+
+# .sel(lat=slice(-20, -10), lon=slice(120, 134)) # west Australia
+# .sel(lat=slice(-1, 5), lon=slice(-52, -44)) # Amazonas Delta
+# .where(coast_mask & ((160 < rome['lon']) | (rome['lon'] < -90)) & (0 < rome['lat']), other=np.nan) # North Pacific
+# map_to_plot = rhlow_p90.sum(dim='time').sel(lat=slice(-1, 5), lon=slice(-52, -44)) # Amazonas Delta
+# map_to_plot = rhlow_p90.sum(dim='time').where(coast_mask & ((160 < rome['lon']) | (rome['lon'] < -90)) & (0 < rome['lat']), other=np.nan)
+
+map_to_plot = precip.squeeze()
+
+# n_roll = 140
+# center_lat = map_to_plot[:, -n_roll-1]['lon'].item() + 180
+# map_to_plot = map_to_plot.roll(shifts={'lon': n_roll}, roll_coords=False)
+
+# exploit that lons are now a view only of the lon-values, i.e. point into its memory
+lons = map_to_plot['lon'].values
+lons -= 160
+lons = xr.where(lons < -180, lons + 360, lons)
+
+fig, ax = make_map(projection=ccrs.PlateCarree(central_longitude=160))
 ax.coastlines()
-map_to_plot.plot(ax=ax, cmap='rainbow')# cmap='BrBG')#, vmin=0. , vmax=0.3)
+map_to_plot.plot(ax=ax, cmap='plasma', vmin=0. , vmax=2.5)# cmap='BrBG')#, )
+# ax.pcolormesh(map_to_plot)#, vmin=0. , vmax=2.5)# cmap='BrBG')#, )
+# ax.collections[0].colorbar.set_label('Count ROME_p90 below 40% RH [1]')
 
-# ax.collections[0].colorbar.set_label('RH_500 [1]')
-ax.collections[0].colorbar.set_label('Count ROME_p90 below 40% RH [1]')
-# ax.collections[0].colorbar.set_label('$\Delta_\mathrm{prox}$ [km$^2$]')
-# ax.collections[0].colorbar.set_label('$\Delta_\mathrm{prox}$ / $\Delta_\mathrm{size}$ [1]')
+plt.savefig(home+'/Desktop/map', bbox_inches='tight', transparent=True)
+# plt.show()
 
 # fig, ax = plt.subplots(figsize=(48, 3))
-# rome_southofindia.plot()
-# ax.axhline(y=np.nanpercentile(rome, q=90), color='r')
+# # rome_southofindia.plot()
+# rome_domain = rome.sel(lat=slice(-1, 5), lon=slice(-52, -44))
+# rome_domain_high = rome_domain.where(rome_domain > np.nanpercentile(rome_domain, q=90), other=np.nan)
+# rome_avg = rome_domain_high.mean(dim='lat').mean(dim='lon')
+# rome_avg.plot()
+# ax.axhline(y=np.nanpercentile(rome_domain, q=90), color='r')
+# plt.title(f'P90 avg. Amazonas-delta, p90_domainpixel={round(np.nanpercentile(rome_domain, q=90))}')
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-
-
-plt.savefig(home+'/Desktop/map14.pdf', bbox_inches='tight', transparent=True)
-# plt.show()
 
 stop = timeit.default_timer()
 print(f'Time used: {stop - start}')
--- Plotscripts/plot_phase_space_simulation.py
+++ Plotscripts/plot_phase_space_simulation.py
@@ -9,19 +9,23 @@ start = timeit.default_timer()
 home = expanduser("~")
 
 # no open_mfdataset here, since dask causes runtime-warning in loop below: "invalid value encountered in true_divide"
-ds_ps = xr.open_dataset(home+'/Desktop/hist1.nc')
+ds_ps = xr.open_dataset(home+'/Desktop/hist.nc')
 
 rome = xr.open_dataarray(home + '/Documents/Data/Simulation/r2b10/rome_14mmhour.nc')
 
-land_sea = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/land_sea_avg.nc')
-# coast_mask =   land_sea < 0.2 # ocean
-coast_mask =  (0.2 < land_sea) & (land_sea < 0.8) # coast
-# coast_mask =   land_sea > 0.8 # land
-coast_mask['lat'] = rome['lat']
-coast_mask['lon'] = rome['lon']
-rome = rome.where(coast_mask, other=np.nan)
+l_pick_surface = False
+if l_pick_surface:
+    land_sea = xr.open_dataarray(home+'/Documents/Data/Simulation/r2b10/land_sea_avg.nc')
+    # coast_mask =   land_sea < 0.2 # ocean
+    coast_mask =  (0.2 < land_sea) & (land_sea < 0.8) # coast
+    # coast_mask =   land_sea > 0.8 # land
+    coast_mask['lat'] = rome['lat']
+    coast_mask['lon'] = rome['lon']
+    rome = rome.where(coast_mask, other=np.nan)
 
-rome = rome.where((120 < rome['lon']) & (rome['lon'] < 134) & (-20 < rome['lat']) & (rome['lat'] < -10), other=np.nan) # West Australia
+# rome = rome.where(((160 < rome['lon']) | (rome['lon'] < -90)) & (0 < rome['lat']), other=np.nan) # North Pacific
+rome = rome.where((-52 < rome['lon']) & (rome['lon'] < -44) & (-1 < rome['lat']) & (rome['lat'] < 5), other=np.nan) # Amazonas delta
+# rome = rome.where((120 < rome['lon']) & (rome['lon'] < 134) & (-20 < rome['lat']) & (rome['lat'] < -10), other=np.nan) # West Australia
 # rome = rome.where((90 < rome['lon']) & (rome['lat']  < 0), other=np.nan) # South MC
 # rome = rome.where((90 < rome['lon']) & (rome['lon'] < 160), other=np.nan) # MC
 # rome = rome.where(rome['lon'] < -90, other=np.nan) # East Pacific
@@ -89,9 +93,9 @@ the_plot = ps_overlay.T.plot(cmap='rainbow', #'gray_r', #'gnuplot2', #'gist_yarg
 plt.xlabel('$\omega_{515}$ [Pa/s]')
 plt.ylabel('RH$_{515}$ [1]')
 
-the_plot.colorbar.set_label('Prob. of coastal ROME$_\mathrm{p90}$ [1]')
+the_plot.colorbar.set_label('Prob. of ROME$_\mathrm{p90}$ [1]')
 
-plt.savefig(home + '/Desktop/phase_space_rome.pdf', transparent=True, bbox_inches='tight')
+plt.savefig(home + '/Desktop/phase_space.pdf', transparent=True, bbox_inches='tight')
 plt.show()
 
 stop = timeit.default_timer()
